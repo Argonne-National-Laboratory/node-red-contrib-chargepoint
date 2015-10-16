50921e8893e9aa6138a5b262674f736f7e66045e
Jason D. Harper <jharper@anl.gov>
2015-10-16 11:13:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+node-red-contrib-chargepoint
+========================
+
+[Node-Red][4] nodes for communicating with the EVSP [ChargePoint][1] allowing monitoring of Chargepoint EVSE and load control.  
+
+Based on the [ChargePoint 4.1 API][2] utilizing the [4.1 WSDL][3].
+
+#Install
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-contrib-chargepoint
+
+
+#Requirements
+
+To access the ChargePoint Web Services API via these nodes, you will require a License Key, Password and a ChargePoint account with one of the service plans described in Section 1.1 of the [ChargePoint API Guide][2].
+
+To find your API License Key and API Password, follow these steps:
+1.Log into ChargePoint with either a Network Manager or API user account
+2.Click the Organizations tab, and then click the API Info sub-tab
+3.Find your API License Key in the table
+4.Mouse over your API License Key, and select Generate Password from the pop-up menu
+5.Save the API License Key and API Password values for making calls to the ChargePoint Web Services API
+
+A Service Provider Plan is necessary to utilize the Demand Management nodes.
+
+#Author
+
+[Jason D. Harper][5] 
+
+
+[1]:http://www.chargepoint.com/
+[2]:https://na.chargepoint.com/UI/downloads/en/ChargePoint_Web_Services_API_Guide_Ver4.1_Rev4.pdf
+[3]:https://webservices.chargepoint.com/cp_api_4.1.wsdl
+[4]:http://nodered.org
+[5]:https://github.com/jayharper
+
+
---
+++ chargepoint/chargepoint.js
@@ -0,0 +1,288 @@
+/**
+ * Copyright 2015 Argonne National Laboratory.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    //"use strict";
+    var RED = require(process.env.NODE_RED_HOME+"/red/red");
+    var settings = RED.settings;
+    var soap     = require('soap');    
+    var url = "https://webservices.chargepoint.com/cp_api_4.1.wsdl"; // URL for ChargePoint wsdl     
+
+    function getLoad(n) {
+        RED.nodes.createNode(this,n);        
+	var node = this;
+	this.name = n.name;	
+	var chargepoint_username = node.credentials.username;
+        var chargepoint_password = node.credentials.password;
+	var cpsgID = n.sgID;  //required
+	var cpstationID = n.stationID;  //optional
+	if (cpstationID){      	 	
+          var getLoad_args = { sgID: parseInt(cpsgID), stationID: cpstationID };	
+	}
+	else{
+	  var getLoad_args = { sgID: parseInt(cpsgID) }; 
+        } 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+                topic: msg.topic
+            };	
+    	    
+            soap.createClient(url, function(err, client) {                                
+             
+                if (client)
+                {
+		    client.setSecurity(new soap.WSSecurity(chargepoint_username,chargepoint_password,'PasswordText'));
+		    node.status({fill:"yellow",shape:"dot",text:"Polling"});		
+                    client.chargepointservices.chargepointservicesSOAP.getLoad(getLoad_args, function(err, result, raw, soapHeader)
+			{	
+			  if(err) {
+			  node.status({fill:"red",shape:"dot",text:"Error"});
+			  console.log(err); 
+			  node.error('ChargePoint ' + err);			  
+			  }
+			  else if(result)
+			  {			  
+			   //console.log(client.lastRequest);		    
+  			   //console.log(result);
+			   node.status({fill:"grey",shape:"dot",text:"Finished"});
+			   if(result.responseCode != "100"){
+			   node.warn(result);
+			   }
+			   else {
+			   outmsg.payload = result;
+			   node.send(outmsg);
+			   //console.log(outmsg);
+			   }
+			  }			  
+			});                    
+                }              
+            });
+        });
+    }
+
+    RED.nodes.registerType("getLoad",getLoad,{
+        credentials: {
+            username: {type:"text"},
+            password: {type: "password"}
+        }
+    });
+
+
+function shedLoad(n) {
+        RED.nodes.createNode(this,n);        
+	var node = this;
+	this.name = n.name;	
+	var chargepoint_username = node.credentials.username;
+        var chargepoint_password = node.credentials.password;
+	/****sgData****/
+	var cpsgID = n.sgID;   //required
+	var cpstationID = n.stationID; //optional 
+	/****sgLoadData****/
+	var cpallowedLoad = n.allowedLoad //optional	
+	var cppercentShed = n.percentShed //optional	
+	var cptimeInterval = n.timeInterval //optional
+	/*********************Input Format*******************************
+	{ sgData: { sgID: 'xsd:int', stationID: 'xsd:string' },
+     	sgLoadData: { allowedLoad: 'xsd:double', percentShed: 'xsd:int' },
+     	timeInterval: 'xsd:int' }
+	****************************************************************/
+
+        node.on("input", function(msg) {
+	    var outmsg = {
+                topic: msg.topic
+            	};
+			
+		if (!cpallowedLoad &&  msg.allowedLoad){ //default to property input but if no input look for passed in allowedLoad
+		cpallowedLoad = msg.allowedLoad;		
+		}
+		if (!cppercentShed && msg.percentShed){ //default to property input but if no input look for passed in percentShed
+		cppercentShed = msg.percentShed;		
+		}
+
+		var shedLoad_args = {sgData: {sgID: parseInt(cpsgID)}}; 
+		  shedLoad_args['sgData']['stationID'] = cpstationID;	
+		if (cpallowedLoad){	
+		  shedLoad_args['sgLoadData'] = {allowedLoad: parseFloat(cpallowedLoad)};
+		}
+		else {
+		  shedLoad_args['sgLoadData'] = {percentShed: parseFloat(cppercentShed)};
+		}
+		shedLoad_args['timeInterval'] = parseInt(cptimeInterval);		
+		//console.log('after:', shedLoad_args);
+
+            soap.createClient(url, function(err, client) {                                
+             
+                if (client)
+                {
+		  //console.log("retrieve", client.describe().chargepointservices.chargepointservicesSOAP.shedLoad);
+		    client.setSecurity(new soap.WSSecurity(chargepoint_username,chargepoint_password,'PasswordText'));
+		    node.status({fill:"yellow",shape:"dot",text:"Polling"});		
+                    client.chargepointservices.chargepointservicesSOAP.shedLoad(shedLoad_args, function(err, result, raw, soapHeader)
+			{	
+			  if(err) {
+			  node.status({fill:"red",shape:"dot",text:"Error"});
+			  console.log(err); 
+			  node.error('ChargePoint ' + err);			  
+			  }
+			  else if(result)
+			  {			  
+			   //console.log(client.lastRequest);		    
+  			   //console.log(result);
+			   node.status({fill:"grey",shape:"dot",text:"Finished"});
+			   if(result.responseCode != "100"){
+			   node.warn(result);
+			   }
+			   else {
+			   outmsg.payload = result;
+			   node.send(outmsg);
+			   //console.log(outmsg);
+			   }
+			  }			  
+			});                   
+                }              
+            });
+        });
+    }
+
+    RED.nodes.registerType("shedLoad",shedLoad,{
+        credentials: {
+            username: {type:"text"},
+            password: {type: "password"}
+        }
+    });
+
+
+	function clearShedState(n) {
+        RED.nodes.createNode(this,n);        
+	var node = this;
+	this.name = n.name;	
+	var chargepoint_username = node.credentials.username;
+        var chargepoint_password = node.credentials.password;
+	var cpsgID = n.sgID;  //required
+	var cpstationID = n.stationID;  //optional
+	if (cpstationID){      	 	
+          var clearShedState_args = { sgID: parseInt(cpsgID), stationID: cpstationID };	
+	}
+	else{
+	  var clearShedState_args = { sgID: parseInt(cpsgID) }; 
+        } 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+                topic: msg.topic
+            };	
+    	    
+            soap.createClient(url, function(err, client) {                                
+             
+                if (client)
+                {
+		    client.setSecurity(new soap.WSSecurity(chargepoint_username,chargepoint_password,'PasswordText'));
+		    node.status({fill:"yellow",shape:"dot",text:"Requesting"});		
+                    client.chargepointservices.chargepointservicesSOAP.clearShedState(clearShedState_args, function(err, result, raw, soapHeader)
+			{	
+			  if(err) {
+			  node.status({fill:"red",shape:"dot",text:"Error"});
+			  console.log(err); 
+			  node.error('ChargePoint ' + err);			  
+			  }
+			  else if(result)
+			  {			  
+			   //console.log(client.lastRequest);		    
+  			   //console.log(result);
+			   node.status({fill:"grey",shape:"dot",text:"Finished"});
+			   if(result.responseCode != "100"){
+			   node.warn(result);
+			   }
+			   else {
+			   outmsg.payload = result;
+			   node.send(outmsg);
+		           //console.log(outmsg);
+			   }
+			  }			  
+			});                    
+                }              
+            });
+        });
+    }
+
+    RED.nodes.registerType("clearShedState",clearShedState,{
+        credentials: {
+            username: {type:"text"},
+            password: {type: "password"}
+        }
+    });
+
+	function getStations(n) {
+        RED.nodes.createNode(this,n);        
+	var node = this;
+	this.name = n.name;	
+	var chargepoint_username = node.credentials.username;
+        var chargepoint_password = node.credentials.password;	
+	var cpstationID = n.stationID;  //optional
+	var getStations_args = {};
+	if (cpstationID){	    	 	
+          getStations_args['searchQuery'] = { stationID: cpstationID };
+	}	
+	
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+                topic: msg.topic
+            };	
+    	    
+            soap.createClient(url, function(err, client) {                                
+             
+                if (client)
+                {
+		    client.setSecurity(new soap.WSSecurity(chargepoint_username,chargepoint_password,'PasswordText'));
+		    node.status({fill:"yellow",shape:"dot",text:"Requesting"});		
+		    //console.log(client.describe());
+		    //console.log("retrieve", client.describe().chargepointservices.chargepointservicesSOAP.getStations);
+                    client.chargepointservices.chargepointservicesSOAP.getStations(getStations_args, function(err, result, raw, soapHeader)
+			{	
+			  if(err) {
+			  node.status({fill:"red",shape:"dot",text:"Error"});
+			  console.log(err); 
+			  node.error('ChargePoint ' + err);			  
+			  }
+			  else if(result)
+			  {			  
+			   //console.log(client.lastRequest);		    
+  			   //console.log(result);
+			   node.status({fill:"grey",shape:"dot",text:"Finished"});
+			   if(result.responseCode != "100"){
+			   node.warn(result);
+			   }
+			   else {
+			   outmsg.payload = result;
+			   node.send(outmsg);
+			   }
+			  }			  
+			});               
+                }              
+            });
+        });
+    }
+
+    RED.nodes.registerType("getStations",getStations,{
+        credentials: {
+            username: {type:"text"},
+            password: {type: "password"}
+        }
+    });
+
+}
